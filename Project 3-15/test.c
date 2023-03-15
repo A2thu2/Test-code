55468417a3be9579f60c83e6e0045a0805edfe23
Arthur <2023046288@qq.com>
2023-03-15 23:11:16 +0800

    修改

--- Project 3-15/test.c
+++ Project 3-15/test.c
@@ -5,23 +5,23 @@
 
 
 
-//int main()
-//{
-//
-//	char input[20] = { 0 };
-//	system("shutdown -s -t 60");
-//	while (1)
-//	{
-//		printf("你的电脑将在一分钟后自动关机，取消关机请输入：我是猪\n请输入：");
-//		scanf("%s", input);
-//		if (strcmp(input, "我是猪") == 0)//strcmp函数比较两个字符串是否相等，相等则为假（值为0）
-//		{
-//			system("shutdown -a");
-//			break;
-//		}
-//	}
-//	return 0;
-//}
+int main()
+{
+
+	char input[20] = { 0 };
+	system("shutdown -s -t 60");
+	while (1)
+	{
+		printf("你的电脑将在一分钟后自动关机，取消关机请输入：我是猪\n请输入：");
+		scanf("%s", input);
+		if (strcmp(input, "我是猪") == 0)//strcmp函数比较两个字符串是否相等，相等则为假（值为0）
+		{
+			system("shutdown -a");
+			break;
+		}
+	}
+	return 0;
+}
 
 
 //int main()
